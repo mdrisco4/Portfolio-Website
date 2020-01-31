d4ea21cc805b8e8546e372b2efbb9e4d6b3adec3
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-01-31 16:58:41 -0500

    working on layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,28 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Michael Driscoll</h1>
-    <p>Welcome to my portfolio</p>
-    <p>Manufacturing innovative solutions in a fast paced industry</p>
+    <div className='me-container' style={{
+      display: 'flex',
+      flexDirection: 'column',
+    }}>
+      <img src='../../photo/photo.jpg' style={{
+        height: '250px',
+        width: '250px',
+      }}></img>
+      <div className='personal-intro-container'>
+        <h1>Intro thing</h1>
+        <p>Welcome to my portfolio</p>
+        <p style={{
+          width: '250px'
+        }}>Engineering innovative solutions in a fast paced industry</p>
+      </div>
+    </div>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <div style={{
         display: 'grid',
-        gridTemplateColumns: '280px 280px 280px',
-        gridTemplateRows: '280px 280px',
+        gridTemplateColumns: '280px 280px',
+        gridTemplateRows: '280px 280px 280px',
+        marginLeft: '320px'
       }}>
       <img src='' className='project-link' style={{
         border: `1px solid black`,
@@ -47,6 +61,21 @@ const IndexPage = () => (
         width: 260,
         height: 260,
       }}></img>
+      {/* <img src='' className='project-link' style={{
+        border: `10px solid red`,
+        width: 260,
+        height: 260,
+      }}></img>
+      <img src='' className='project-link' style={{
+        border: `1px solid black`,
+        width: 260,
+        height: 260,
+      }}></img>
+      <img src='' className='project-link' style={{
+        border: `1px solid black`,
+        width: 260,
+        height: 260,
+      }}></img> */}
       </div>
       {/* <Image /> */}
     </div>
