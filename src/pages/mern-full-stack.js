9c47a864ab6f765a83a0d4cd9f269dbe96cc2a38
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-02-27 00:29:56 -0500

    started adding content to the breweries project page.  still need to drop in pertinent images and style

--- src/pages/mern-full-stack.js
+++ src/pages/mern-full-stack.js
@@ -37,19 +37,18 @@ const DeployedLink = styled.a`
   font-size: 20px;
   background-color: rgb(99, 95, 85);
   border: 4px solid darkblue;
-  border-radius: 8px;
   height: auto;
   padding: 4px 0;
-  color: "black";
-  text-shadow: 2px 2px 5px red;
+  color: white;
   text-decoration: none;
   margin-bottom: 24px;
 @media (min-width: 1000px) {
   width: 42%;
 }
   &:hover {
+    color: rgb(110, 176, 249);
+    background-color: darkblue;
     border: 4px solid black;
-    background-color: rgb(90, 176, 255);
   }
 `
 
@@ -60,19 +59,18 @@ const GitHubLink = styled.a`
   font-size: 20px;
   background-color: rgb(99, 95, 85);
   border: 4px solid darkblue;
-  border-radius: 8px;
   height: auto;
   padding: 4px 0;
-  color: "black";
-  text-shadow: 2px 2px 5px red;
+  color: white;
   text-decoration: none;
   margin-bottom: 24px;
 @media (min-width: 1000px) {
   width: 42%;
 }
   &:hover {
+    color: rgb(110, 176, 249);
+    background-color: darkblue;
     border: 4px solid black;
-    background-color: rgb(90, 176, 255);
   }
 `
 
@@ -81,6 +79,10 @@ const Title = styled.h1`
   width: 80%;
   margin-left: auto;
   margin-right: auto;
+`
+
+const SectionTitle = styled.div`
+  font-size: 36px;
 `
 
 const ProjectDescription = styled.p`
@@ -96,20 +98,20 @@ const RocBreweries = () => (
     <Container>
       <Title>Rochester Craft Beer Community</Title>
       <ProjectLinkContainer>
-      <DeployedLink
-        href="http://54.161.227.204:8000/breweries/"
-        target="_blank"
+        <DeployedLink
+          href="http://54.161.227.204:8000/breweries/"
+          target="_blank"
         >
-        ROC BREWERIES
-      </DeployedLink>
-      <GitHubLink
-      href="https://github.com/mdrisco4/breweries_django_be"
-      target="_blank"
-      >
-        GITHUB REPO
-      </GitHubLink>
+          ROC BREWERIES
+        </DeployedLink>
+        <GitHubLink
+          href="https://github.com/mdrisco4/breweries_django_be"
+          target="_blank"
+        >
+          GITHUB REPO
+        </GitHubLink>
       </ProjectLinkContainer>
-      <h3>Description</h3>
+      <SectionTitle>Description</SectionTitle>
       <ProjectDescription>
         The idea behind this project was to develop a full stack application
         with admin logins and passwords to be used for different CRUD
@@ -119,9 +121,28 @@ const RocBreweries = () => (
         have restricted access to admin permissions to delete or force any
         changes needed. This was developed with Python and Django.
       </ProjectDescription>
-      <h3>Approach</h3>
-      <h3>Problems</h3>
-      <h3>Solutions</h3>
+      <SectionTitle>Approach</SectionTitle>
+      <ProjectDescription>
+        I developed this project using Python with a Django backend and Django
+        templates to build out my front end. This was a project in which I
+        wanted to employ a new technology that I had not used before.
+        Additionally the scale of the project did not require a complex stack
+        build, so using templates made sense, both for simplicity and to learn
+        how to write a Django app more thoroughly. The bulk of the work required
+        was in constructing my data models and creating a backend with full
+        stack capabilities. The use of Django templates would make building the
+        front end a fairly quick and straightforward process.
+      </ProjectDescription>
+      <SectionTitle>Problems</SectionTitle>
+      <ProjectDescription>
+        The most difficult and involved part of building this app was setting up
+        admin permissions and permissions for verified users (i.e. brewery
+        owners, managers, etc.) the ability to access full CRUD capabilities so
+        as to not allow anyone to go to the deployed site and poke around with
+        the content.
+      </ProjectDescription>
+      <SectionTitle>Solutions</SectionTitle>
+      <ProjectDescription></ProjectDescription>
     </Container>
   </Layout>
 )
