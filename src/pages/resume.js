ee6f7ed407b90b2e27134ad4cb08cfefece9ef1c
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-02-27 22:14:31 -0500

    close to finishing resume page layout, need higher resolution PDF for the PDFlink

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -4,34 +4,25 @@ import React from "react"
 import Layout from "../components/layout"
 import styled from "styled-components"
 
-const ContactInfo = styled.div`
-  text-align: center;
-  font-size: 35px;
-  font-weight: bold;
-  @media (min-width: 600px) {
-    font-size: 50px;
-  }
-`
-
-// const Questions = styled.div`
-//   width: 70%;
-//   text-align: center;
-//   margin: auto;
-//   margin-bottom: 30px;
-//   font-size: 24px;
-//   @media (min-width: 600px) {
-//     font-size: 28px;
-//   }
-// `
 
 const Container = styled.div`
   @media (min-width: 800px) {
       border: 1px solid green;
-      width: 85%;
+      width: 80%;
       margin-left: auto;
-  margin-right: auto;
-  }
-`
+      margin-right: auto;
+    }
+    `
+
+    const AboutMe = styled.div`
+      @media (min-width: 600px) {
+      }
+    `
+    
+    const ContactInfo = styled.div`
+      @media (min-width: 600px) {
+      }
+    `
 
 const MyInfo = styled.div`
   width: 80%;
@@ -56,27 +47,61 @@ const Name = styled.h1`
 `
 
 const SectionTitle = styled.div`
-        font-size: 24px;
-        font-weight: bold;
-    @media (min-width: 800px) {
-        font-size: 32px;
-        font-weight: bold;
-    }
+  font-size: 24px;
+  font-weight: bold;
+  @media (min-width: 800px) {
+    font-size: 32px;
+    font-weight: bold;
+  }
+`
+
+const SkillsContainer = styled.div`
+  @media (min-width: 800px) {
+    display: flex;
+  }
+`
+
+const SkillsRowContainer = styled.div`
+  @media (min-width: 800px) {
+    margin-right: 40px;
+  }
 `
 
 const SubSectionTitle = styled.div`
-        font-size: 20px;
-        font-weight: bold;
-    @media (min-width: 800px) {
-        font-size: 24px;
-        font-weight: bold;
-    }`
+  font-size: 20px;
+  font-weight: bold;
+  @media (min-width: 800px) {
+    font-size: 24px;
+    font-weight: bold;
+  }
+`
+
+const City = styled.div`
+font-size: 18px;
+  @media (min-width: 600px) {
+    font-size: 20px;
+    font-weight: bold;
+  }
+`
+
+const Dates = styled.div`
+font-size: 18px;
+  @media (min-width: 600px) {
+    font-size: 20px;
+    font-weight: bold;
+  }
+`
 
 const Details = styled.div`
-        font-size: 18px;
- @media (min-width: 800px) {
-        font-size: 20px;
-    }`
+  font-size: 18px;
+  @media (min-width: 800px) {
+    font-size: 20px;
+  }
+`
+
+const BulletPointWrap = styled.div`
+    display: flex;
+`
 
 const ContactDetails = styled.h3`
   margin: 4px 0;
@@ -85,6 +110,13 @@ const ContactDetails = styled.h3`
     font-size: 30px;
   }
 `
+
+const BulletPoints = styled.div`
+font-size: 18px;
+  @media (min-width: 800px) {
+    font-size: 20px;
+    margin-left: 12px;
+}`
 
 const PDFLink = styled.a`
   font-size: 25px;
@@ -142,80 +174,121 @@ const LinkedInLink = styled.a`
 
 const Resume = () => (
   <Layout>
-      <Container>
-    <Name>Michael Driscoll</Name>
-    <SectionTitle>Software Developer</SectionTitle>
-    <Details>about me blurb</Details>
-    <SectionTitle>Contact Info</SectionTitle>
-    <Details>585-719-7720</Details>
-    <EmailLink href="mailto:michaelndriscoll81@gmail.com" target="_blank">
-      michaelndriscoll81@gmail.com
-    </EmailLink><br />
-    <LinkedInLink
-      href="https://www.linkedin.com/in/michael-n-driscoll/"
-      target="_blank"
-    >
-      linkedin.com/in/michael-n-driscoll/
-    </LinkedInLink>
-    <PDFLink href="/resume-hard-copy">PDF Version</PDFLink>
-    <SectionTitle>Skills</SectionTitle>
-    <div
-      style={{
-        display: "flex",
-      }}
-    >
+    <Container>
+      <Name>Michael Driscoll</Name>
       <div
         style={{
-          marginRight: "40px",
+          display: "flex",
         }}
       >
-        <SubSectionTitle>∙ HTML/CSS</SubSectionTitle>
-        <SubSectionTitle>∙ Javascript</SubSectionTitle>
-        <SubSectionTitle>∙ Node.js</SubSectionTitle>
+        <AboutMe>
+          <SectionTitle>Software Developer</SectionTitle>
+          <Details>about me blurb</Details>
+        </AboutMe>
+        <ContactInfo>
+          <SectionTitle>Contact Info</SectionTitle>
+          <Details>585-719-7720</Details>
+          <EmailLink href="mailto:michaelndriscoll81@gmail.com" target="_blank">
+            Email
+          </EmailLink>
+          <br />
+          <LinkedInLink
+            href="https://www.linkedin.com/in/michael-n-driscoll/"
+            target="_blank"
+          >
+            LinkedIn
+          </LinkedInLink>
+        </ContactInfo>
       </div>
-      <div
-        style={{
-          marginRight: "40px",
-        }}
-      >
-        <SubSectionTitle>∙ Express</SubSectionTitle>
-        <SubSectionTitle>∙ React</SubSectionTitle>
-        <SubSectionTitle>∙ SQL</SubSectionTitle>
-      </div>
-      <div
-        style={{
-          marginRight: "40px",
-        }}
-      >
-        <SubSectionTitle>∙ Adobe XD</SubSectionTitle>
-        <SubSectionTitle>∙ Python/Django</SubSectionTitle>
-        <SubSectionTitle>∙ MongoDB</SubSectionTitle>
-      </div>
-    </div>
-    <SectionTitle>Experience</SectionTitle>
-    <SubSectionTitle>Software Engineering Immersive, General Assembly</SubSectionTitle>
-    <Details>Washington, D.C.</Details>
-    <Details>11/2019-2/2020</Details>
-    <Details>
-      Completed a 500 hour full stack immersive program and designed projects
-      including:
-    </Details>
-    <SubSectionTitle>Front End Team Member, WhichCraft Brewing</SubSectionTitle>
-    <Details>Rochester, NY</Details>
-    <Details>3/2019-11/2019</Details>
-    <SubSectionTitle>Front End Team Member, The Beer Market</SubSectionTitle>
-    <Details>Rochester, NY</Details>
-    <Details>4/2015-11/2019</Details>
-    <SubSectionTitle>Image Science Department Intern, Exelis Geospatial Systems</SubSectionTitle>
-    <Details>Rochester, NY</Details>
-    <Details>6/2013-8/2013</Details>
-    <SectionTitle>Education</SectionTitle>
-    <SubSectionTitle>Full Stack Web Development Certificate, General Assembly</SubSectionTitle>
-    <Details>Washington, D.C.</Details>
-    <Details>11/2019-2/2020</Details>
-    <SubSectionTitle>Bachelor of Arts in Physics, University of Rochester</SubSectionTitle>
-    <Details>Rochester, NY</Details>
-    <Details>1/2010-5/2013</Details>
+      <PDFLink href="/resume-hard-copy">PDF Version</PDFLink>
+      <SectionTitle>Skills</SectionTitle>
+      <SkillsContainer>
+        <SkillsRowContainer>
+          <SubSectionTitle>∙ HTML/CSS</SubSectionTitle>
+          <SubSectionTitle>∙ Javascript</SubSectionTitle>
+          <SubSectionTitle>∙ Node.js</SubSectionTitle>
+        </SkillsRowContainer>
+        <SkillsRowContainer>
+          <SubSectionTitle>∙ Express</SubSectionTitle>
+          <SubSectionTitle>∙ React</SubSectionTitle>
+          <SubSectionTitle>∙ SQL</SubSectionTitle>
+        </SkillsRowContainer>
+        <SkillsRowContainer>
+          <SubSectionTitle>∙ Adobe XD</SubSectionTitle>
+          <SubSectionTitle>∙ Python/Django</SubSectionTitle>
+          <SubSectionTitle>∙ MongoDB</SubSectionTitle>
+        </SkillsRowContainer>
+      </SkillsContainer>
+      <SectionTitle>Experience</SectionTitle>
+      <SubSectionTitle>
+        Software Engineering Immersive, General Assembly
+      </SubSectionTitle>
+      <City>Washington, D.C.</City>
+      <Dates>11/2019-2/2020</Dates>
+      <Details>
+        Completed a 500 hour full stack immersive program and designed projects
+        including:
+      </Details>
+      <SubSectionTitle>
+        Front End Team Member, WhichCraft Brewing
+      </SubSectionTitle>
+      <City>Rochester, NY</City>
+      <Dates>3/2019-11/2019</Dates>
+      <BulletPointWrap>
+      <Details>∙</Details>
+      <BulletPoints>
+        Provided experienced feedback and recommendations for weekly
+        introductions of new and unique products manufactured at our on site
+        production brewery helping to boost sales for inhouse products, got
+        positive feedback and provided an all around enjoyable experience
+      </BulletPoints>
+      </BulletPointWrap>
+      <SubSectionTitle>Front End Team Member, The Beer Market</SubSectionTitle>
+      <City>Rochester, NY</City>
+      <Dates>4/2015-11/2019</Dates>
+      <BulletPointWrap>
+      <Details>∙</Details>
+      <BulletPoints>
+        Developed relationships in the industry with brewery owners and
+        distribution representatives to promote 300+ products and host more than
+        100+ industry events with attendance ranging from dozens to hundreds of
+        customers and craft beer professionals
+      </BulletPoints>
+        </BulletPointWrap>
+      <SubSectionTitle>
+        Image Science Department Intern, Exelis Geospatial Systems
+      </SubSectionTitle>
+      <City>Rochester, NY</City>
+      <Dates>6/2013-8/2013</Dates>
+      <BulletPointWrap>
+      <Details>∙</Details>
+      <BulletPoints>
+        Executed objectives for three contract assignments to build software
+        with Mathematica and Matlab that 1) modeled optical systems and 2)
+        updated GPS technology with adjustments for perpetual, small changes of
+        the Earth’s axial tilt
+      </BulletPoints>
+      </BulletPointWrap>
+      <BulletPointWrap>
+      <Details>∙</Details>
+      <BulletPoints>
+        Conducted extensive research on satellite optical systems and GPS
+        synchronization in the Image Science Department at Exelis, a leading
+        aerospace and defense contractor, to develop analytical software tools
+        for deployment in government programs
+      </BulletPoints>
+      </BulletPointWrap>
+      <SectionTitle>Education</SectionTitle>
+      <SubSectionTitle>
+        Full Stack Web Development Certificate, General Assembly
+      </SubSectionTitle>
+      <Details>Washington, D.C.</Details>
+      <Dates>11/2019-2/2020</Dates>
+      <SubSectionTitle>
+        Bachelor of Arts in Physics, University of Rochester
+      </SubSectionTitle>
+      <Details>Rochester, NY</Details>
+      <Details>1/2010-5/2013</Details>
     </Container>
   </Layout>
 )
