6d7310198e4e71db2d64f328f64be9c3584d4e4c
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-06-24 12:40:38 -0400

    working on changing some gatsby config file settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `| Portfolio`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
