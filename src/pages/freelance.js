92967f5b7eb43f7ccfe6430d2ee00dde49e85f3e
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-07-22 12:05:02 -0400

    most of needed content written out, needs fine tuning

--- src/pages/freelance.js
+++ src/pages/freelance.js
@@ -39,26 +39,99 @@ const ServicesInfo = styled.div`
   width: 80%;
 `
 
-const ContactInfo = styled.div`
-  font-family: "Montserrat Alternates";
-  text-align: center;
-  font-size: 35px;
-  font-weight: bold;
-  @media (min-width: 600px) {
-    font-size: 50px;
+const ImageWrapper = styled.div`
+  position: relative;
+  width: 55%;
+  height: auto;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 40px;
+  &:hover {
+    cursor: pointer;
   }
 `
 
-const Questions = styled.div`
+const ProjectStill = styled.img`
+  width: 100%;
+  height: auto;
+  display: inline-block;
+  margin-left: auto;
+  margin-right: auto;
+`
+
+const ProjectDescription = styled.div`
   font-family: "Montserrat Alternates";
-  width: 70%;
+  display: none;
+  @media (min-width: 750px) {
+    display: block;
+    text-align: center;
+    font-size: 21px;
+    position: absolute;
+    bottom: 2px;
+    right: 0;
+    background: black;
+    color: white;
+    opacity: 0;
+    width: 100%;
+    height: 100%;
+
+    -webkit-transition: visibility 0s, opacity 0.5s linear;
+    transition: visibility 0s, opacity 0.5s linear;
+    &:hover {
+      width: 100%;
+      visibility: visible;
+      opacity: 0.7;
+    }
+  }
+`
+
+const MobileProjectDescription = styled.div`
+  font-family: "Montserrat Alternates";
+  display: flex;
+  width: 75%;
+  font-size: 20px;
   text-align: center;
-  margin: auto;
+  margin-left: auto;
+  margin-right: auto;
   margin-bottom: 30px;
-  font-size: 24px;
-  @media (min-width: 600px) {
-    font-size: 28px;
-    max-width: 800px;
+  justify-content: center;
+  @media (min-width: 750px) {
+    display: none;
+  }
+`
+
+const ProjectTitle = styled.div`
+  font-family: "Montserrat Alternates";
+  padding-left: 7.5%;
+  padding-right: 7.5%;
+  text-align: center;
+  font-weight: bold;
+  font-size: 42px;
+  margin-bottom: 20px;
+`
+
+const ProjectLink = styled.a`
+  font-family: "Montserrat Alternates";
+  text-decoration: none;
+  font-size: 25px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center;
+  margin-bottom: 40px;
+  width: 20%;
+  height: auto;
+  padding: 6px;
+  border: 4px solid darkblue;
+  background-color: rgb(99, 95, 85);
+  color: white;
+  cursor: pointer;
+  @media (min-width: 620px) {
+  }
+  &:hover {
+    border: 4px solid black;
+    background-color: darkblue;
+    color: rgb(110, 176, 249);
   }
 `
 
@@ -87,21 +160,30 @@ const MyInfo = styled.div`
   }
 `
 
-const Name = styled.h3`
-  font-family: "Montserrat Alternates";
-  margin: 0 0 8px 0;
-  font-size: 35px;
-  @media (min-width: 800px) {
-    font-size: 50px;
-  }
-`
-
 const ContactDetails = styled.h3`
   font-family: "Montserrat Alternates";
   margin: 4px 0;
   font-size: 24px;
   @media (min-width: 800px) {
     font-size: 30px;
+  }
+`
+
+const PhoneLink = styled.a`
+  font-family: "Montserrat Alternates";
+  color: black;
+  text-shadow: 1px 1px 1px white;
+  text-decoration: none;
+  font-weight: 900;
+  margin: 4px 5%;
+  font-size: 24px;
+  @media (min-width: 600px) {
+    font-size: 30px;
+  }
+  &:hover {
+    color: darkblue;
+    text-shadow: 4px 4px 5px darkblue;
+    filter:drop-shadow(1px 1px white);
   }
 `
 
@@ -200,7 +282,69 @@ const Freelance = () => (
       <ServicesInfo></ServicesInfo>
     </PointWrapper> */}
     <SectionTitle>Projects</SectionTitle>
+    <ProjectTitle>Modern Troussaeu</ProjectTitle>
+    <ImageWrapper>
+      <ProjectStill src="https://i.imgur.com/DOJXKHR.png"></ProjectStill>
+      <ProjectDescription>
+        An open source client project built with React, Contentful and GraphQL
+        on an Agile team. I provided code for the front end working on a team
+        utilizing Storybook for component tracking and git branch management
+        during development.
+      </ProjectDescription>
+    </ImageWrapper>
+    <MobileProjectDescription>
+      An open source client project built with React, Contentful and GraphQL on
+      an Agile team. I provided code for the front end working on a team
+      utilizing Storybook for component tracking and git branch management
+      during development.
+    </MobileProjectDescription>
+    <ProjectLink href="/modern-trousseau">Learn More</ProjectLink>
+
+    <ProjectTitle>Photography Site</ProjectTitle>
+    <ImageWrapper>
+      <ProjectStill src="https://i.imgur.com/s2xoqpd.png"></ProjectStill>
+      <ProjectDescription>
+        A freelance project I made for an aspiring professional photographer. I
+        built this project with a Gatsby frontend and employed the Contentful
+        content management system. With Contentful I authorized the client to
+        add, delete or make changes to photos displayed in their portfolio. With
+        a dynamic data fetch this allows the client to continue to update with a
+        user friendly drag and drop interface
+      </ProjectDescription>
+    </ImageWrapper>
+    <MobileProjectDescription>
+      A freelance project I made for an aspiring professional photographer. I
+      built this project with a Gatsby frontend and employed the Contentful
+      content management system. With Contentful I authorized the client to add,
+      delete or make changes to photos displayed in their portfolio. With a
+      dynamic data fetch this allows the client to continue to update with a
+      user friendly drag and drop interface
+    </MobileProjectDescription>
+    <ProjectLink href="/fragnoli-photos">Learn More</ProjectLink>
     <SectionTitle>Contact info</SectionTitle>
+    <Container>
+      <MyInfo>
+        <PhoneLink href="tel:1-585-719-7720">1-585-719-7720</PhoneLink>
+        <ContactDetails>Washington, DC 20005</ContactDetails>
+        <EmailLink href="mailto:michaelndriscoll81@gmail.com" target="_blank">
+        <FontAwesomeIcon icon={["far", "envelope"]} size="3x" />
+        </EmailLink>
+        <LinkedInLink
+          href="https://www.linkedin.com/in/michael-n-driscoll/"
+          target="_blank"
+        >
+          <FontAwesomeIcon icon={["fab", "linkedin"]} size="3x" />
+        </LinkedInLink>
+        <GitHubLink href="https://github.com/mdrisco4" target="_blank">
+        <FontAwesomeIcon icon={["fab", "github"]} size="3x" />
+        </GitHubLink>
+      </MyInfo>
+    </Container>
+    <hr
+      style={{
+        border: "1px solid black",
+      }}
+    ></hr>
   </Layout>
 )
 
